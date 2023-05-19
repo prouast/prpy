5ca1221deb3a4dcf6baec49681f893e8b10b49b9
Philipp Rouast <philipp@rouast.com>
2023-05-19 11:54:12 +1000

    Separate max delta in factor search

--- propy/ffmpeg/utils.py
+++ propy/ffmpeg/utils.py
@@ -9,10 +9,14 @@ import ffmpeg
 import itertools
 import logging
 
-def find_factors_near(i, f1, f2, f3, max_delta):
-  for delta in range(max_delta+1):
+def find_factors_near(i, f1, f2, f3, max_delta_1, max_delta_2, max_delta_3):
+  # Iterative deepening
+  for delta in range(max(max_delta_1, max_delta_2, max_delta_3)):
+    delta_1 = min(delta, max_delta_1)
+    delta_2 = min(delta, max_delta_2)
+    delta_3 = min(delta, max_delta_3)
     ts = [(t1, t2, t3) for t1, t2, t3 in list(itertools.product( \
-      range(f1-delta, f1+delta+1), range(f2-delta, f2+delta+1), range(f3-delta, f3+delta+1)))]
+      range(f1-delta_1, f1+delta_1+1), range(f2-delta_2, f2+delta_2+1), range(f3-delta_3, f3+delta_3+1)))]
     for t1, t2, t3 in ts:
       if t1 * t2 * t3 == i:
         return t1, t2, t3
