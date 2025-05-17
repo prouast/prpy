b162cee0d9d6c0095a7b070a6ce951d174d90612
Philipp Rouast <philipp@rouast.com>
2025-05-17 15:57:28 +1000

    Move basic functions to core.py

--- tests/test_numpy_signal.py
+++ tests/test_numpy_signal.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2024 Philipp Rouast
+# Copyright (c) 2025 Philipp Rouast
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -21,65 +21,12 @@
 import sys
 sys.path.append('../prpy')
 
-from prpy.numpy.signal import div0, normalize, standardize, moving_average, moving_average_size_for_response, moving_std, detrend
+from prpy.numpy.signal import moving_average, moving_average_size_for_response, moving_std, detrend
 from prpy.numpy.signal import interpolate_vals, interpolate_linear_sequence_outliers, interpolate_data_outliers, interpolate_filtered
 from prpy.numpy.signal import _component_periodicity, select_most_periodic
 
 import numpy as np
 import pytest
-
-@pytest.mark.parametrize("fill", [np.nan, np.inf, 0])
-def test_div0(fill):
-  # Check division by array including 0 with broadcasting
-  a = np.array([[0, 1, 2], [3, 4, 5]])
-  b = np.array([1, 0, 2])
-  a_copy = a.copy()
-  b_copy = b.copy()
-  np.testing.assert_allclose(
-    div0(a=a, b=b, fill=fill),
-    np.array([[0, fill, 1], [3, fill, 2.5]]))
-  # No side effects
-  np.testing.assert_equal(a, a_copy)
-  np.testing.assert_equal(b, b_copy)
-
-def test_normalize():
-  # Check with axis=-1
-  np.testing.assert_allclose(
-    normalize(x=np.array([[0., 1., -3., 2., -1.], [0., 5., -3., 2., 1.]]), axis=-1),
-    np.array([[0.2, 1.2, -2.8, 2.2, -0.8], [-1, 4, -4, 1, 0]]))
-  # Check with axis=0
-  np.testing.assert_allclose(
-    normalize(x=np.array([[0., 1., -3., 2., -1.], [0., 5., -3., 2., 1.]]), axis=0),
-    np.array([[0., -2., 0., 0., -1.], [0., 2., 0., 0., 1.]]))
-  # Check with axis=(0,1)
-  x = np.array([[0., 1., -3., 2., -1.], [0., 5., -3., 2., 1.]])
-  x_copy = x.copy()
-  np.testing.assert_allclose(
-    normalize(x=x, axis=(0,1)),
-    np.array([[-.4, .6, -3.4, 1.6, -1.4], [-.4, 4.6, -3.4, 1.6, .6]]))
-  # No side effects
-  np.testing.assert_equal(x, x_copy)
-
-def test_standardize():
-  # Check with axis=-1
-  np.testing.assert_allclose(
-    standardize(x=np.array([[0., 1., -3., 2., -1.], [0., 5., -3., 2., 1.]]), axis=-1),
-    np.array([[.116247639, .697485834, -1.627466946, 1.278724029, -.464990556],
-              [-.383482495, 1.533929979, -1.533929979, .383482495, 0.]]))
-  # Check with axis=0
-  np.testing.assert_allclose(
-    standardize(x=np.array([[0., 1., -3., 2., -1.], [0., 5., -3., 2., 1.]]), axis=0),
-    np.array([[0., -1., 0., 0., -1.], [0., 1., 0., 0., 1.]]))
-  # Check with axis=(0,1)
-  np.testing.assert_allclose(
-    standardize(x=np.array([[0., 1., -3., 2., -1.], [0., 5., -3., 2., 1.]]), axis=(0,1)),
-    np.array([[-.174740811, .262111217, -1.485296896, .698963245, -.61159284],
-              [-.174740811, 2.00951933, -1.485296896, .698963245, .262111217]]))
-  # No side effect
-  x = np.array([[0., 1., -3., 2., -1.], [0., 5., -3., 2., 1.]])
-  x_copy = x.copy()
-  standardize(x)
-  np.testing.assert_equal(x, x_copy)
 
 MA_ARR = np.array([
   [0.,  .4,  1.2,  2.,   .2 ],
