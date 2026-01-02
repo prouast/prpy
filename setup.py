4de8b5dc48d7b645ab28f543ad219740c31bef08
Philipp Rouast <philipp@rouast.com>
2026-01-02 13:36:09 +1100

    Attempt fix for NumPy 2.0

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ import numpy as np
 from setuptools import setup, Extension
 
 setup(
-  ext_modules=[
+    ext_modules=[
         Extension(
             name="prpy.numpy.image_ops",
             sources=["prpy/numpy/image_ops.c"],
-            include_dirs=[np.get_include()]
+            include_dirs=[np.get_include()],
+            define_macros=[("NPY_TARGET_VERSION", "NPY_1_24_API_VERSION")],
         ),
     ]
 )
