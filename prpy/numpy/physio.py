950c8648c712ebe5f32b9cfe4985a66ebe6efb15
Philipp Rouast <philipp@rouast.com>
2026-03-22 08:42:26 +1100

    Wrap face roi logic from vitallens-core; Deprecate numpy.physio

--- prpy/numpy/physio.py
+++ prpy/numpy/physio.py
@@ -18,6 +18,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+# TODO: File is deprecated and to be removed.
+
 from enum import IntEnum
 import numpy as np
 _trapz = getattr(np, 'trapezoid', getattr(np, 'trapz', None))
